622d80874611d6b93c35d278b4853ff099c71d82
Fahim <fahimshareear@gmail.com>
2026-02-10 11:52:22 +0600

    fixing error for deployment

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         const db = client.db("etuitionBD");
         const tutionsCollection = db.collection("tuitions");
         const usersCollectin = db.collection("users");
